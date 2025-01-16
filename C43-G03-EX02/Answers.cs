9b3040b08a6b24d587cb06142fdb6e962b66b932
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-16 15:37:24 +0200

    answer

---
+++ C43-G03-EX02/Answers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G03_EX02
+{
+    internal class Answers
+    {
+        public int AnswerId { get; set; }
+
+        public string AnswerText { get; set; }
+        public Answers()
+        {
+            AnswerText=string.Empty;
+        }
+
+    }
+}
