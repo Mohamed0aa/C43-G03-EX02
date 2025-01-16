64734a09270defbb4c1ecce6e57bf6038d8bc1e7
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-16 15:37:35 +0200

    subject

---
+++ C43-G03-EX02/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G03_EX02
+{
+    internal class Subject
+    {
+        public int Subject_Id { get; set; }
+        public string Subject_Name { get; set; }
+        public Exam Subject_Exam { get; set; }
+ 
+        public Subject(int id, string name)
+        {
+            Subject_Id = id;
+            Subject_Name = name;
+        }
+        public void Create_Exam (Exam exam)
+        {
+            Subject_Exam = exam;
+        }
+      
+    }
+}
